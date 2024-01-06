2189aa9254b5dcfcd524224ac255d228017f24cb
sueselle <sue@thehappyuser.com>
2024-01-06 10:46:53 -0800

    added closing functionality and more new recipe code

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from "react";
 import Header from "./components/Header";
 import RecipeExcerpt from "./components/RecipeExcerpt";
 import RecipeFull from "./components/RecipeFull";
+import NewRecipeForm from "./components/NewRecipeForm";
 import "./App.css";
 
 
@@ -9,6 +10,15 @@ import "./App.css";
 function App() {
   const [recipes, setRecipes] = useState([]);
   const [selectedRecipe, setSelectedRecipe] = useState(null);
+  const [newRecipe, setNewRecipe] = useState({
+    title: "",
+    ingredients: "",
+    instructions: "",
+    servings: 1, // conservative default
+    description: "",
+    image_url: "https://images.pexels.com/photos/9986228/pexels-photo-9986228.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" //default
+  });
+  const [showNewRecipeForm, setShowNewRecipeForm] = useState(false)
 
   useEffect(() => {
     const fetchAllRecipes = async () => {
@@ -24,11 +34,45 @@ function App() {
       }
         catch (e) {
           console.error("An error occurred during the request:", e);
-          console.log("An unexpected error occurred. Please try again.")
         }
       };
       fetchAllRecipes();
     }, []);
+
+    const handleNewRecipe = async (e, newRecipe) => {
+      e.preventDefault();
+      try {
+        const response = await fetch("/api/recipes", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(newRecipe)
+        });
+        if (response.ok) {
+          const data = await response.json();
+  
+          setRecipes([...recipes, data.recipe]);
+  
+          console.log("Recipe added successfully!");
+  
+          setShowNewRecipeForm(false);
+          setNewRecipe({
+            title: "",
+            ingredients: "",
+            instructions: "",
+            servings: 1,
+            description: "",
+            image_url:
+              "https://images.pexels.com/photos/9986228/pexels-photo-9986228.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          });
+        } else {
+          console.error("Oops - could not add recipe!");
+        }
+      } catch (e) {
+        console.error("An error occurred during the request:", e);
+      }
+    };
 
     const handleSelectRecipe = (recipe) => {
       setSelectedRecipe(recipe);
@@ -36,11 +80,24 @@ function App() {
     const handleUnselectRecipe = () => {
       setSelectedRecipe(null);
     };
+    
+    const hideRecipeForm = () => {
+        setShowNewRecipeForm(false);
+    };
+    const showRecipeForm = () => {
+      setShowNewRecipeForm(true);
+      setSelectedRecipe(null);
+    };
+    const onUpdateForm = (e) => {
+      const { name, value } = e.target;
+      setNewRecipe({...newRecipe, [name]: value });
+    };
   return (
     <div className='recipe-app'>
-      <Header />
+      <Header showRecipeForm={showRecipeForm} />
+      {showNewRecipeForm && <NewRecipeForm newRecipe={newRecipe} hideRecipeForm={hideRecipeForm} onUpdateForm = {onUpdateForm} handleNewRecipe={handleNewRecipe}/>}
       {selectedRecipe && <RecipeFull selectedRecipe={selectedRecipe} handleUnselectRecipe={handleUnselectRecipe} />}
-      {!selectedRecipe && (
+      {!selectedRecipe && !showNewRecipeForm &&(
       <div className = 'recipe-list'>
         {recipes.map((recipe) => (
           <RecipeExcerpt key={recipe.id} recipe={recipe} handleSelectRecipe={handleSelectRecipe}/>
@@ -49,6 +106,7 @@ function App() {
       )}
     </div>
   );
-}
+};
+
 
 export default App;
